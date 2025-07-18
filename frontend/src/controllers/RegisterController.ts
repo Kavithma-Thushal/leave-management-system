ea1e84f0a551b6a53fad699c805307848d7f56ab
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2025-07-18 11:25:59 +0530

    Refactored 2

--- frontend/src/controllers/RegisterController.ts
+++ frontend/src/controllers/RegisterController.ts
@@ -1,11 +1,13 @@
 import axios from 'axios';
 import {useState} from 'react';
+import { useNavigate } from 'react-router-dom';
 import {successNotification, errorNotification} from '../util/Alert';
 
 const BASE_URL = 'http://127.0.0.1:8000/api/v1';
 
 export default function registerController() {
     const [form, setForm] = useState({name: '', email: '', password: ''});
+    const navigate = useNavigate();
 
     const handleChange = (e: any) => {
         setForm({...form, [e.target.name]: e.target.value});
@@ -16,7 +18,7 @@ export default function registerController() {
         try {
             const response = await axios.post(`${BASE_URL}/register`, form);
             successNotification(response.data.message);
-            setForm({name: '', email: '', password: ''});
+            navigate('/login');
         } catch (error: any) {
             errorNotification(Object.values(error.response?.data?.error));
         }
