d88b59362bb6af8cf5e6a0eb6fe0ba788ebdfee2
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2025-07-18 12:32:36 +0530

    ApplyForLeave Updated 2

--- frontend/src/views/pages/AdminDashboard.tsx
+++ frontend/src/views/pages/AdminDashboard.tsx
@@ -1,9 +1,7 @@
 export default function EmployeeDashboard() {
     return (
-        <div className="min-h-screen flex flex-col items-center justify-center bg-[#000a1f] px-4">
-            <h1 className="text-5xl font-extrabold text-neonBlue drop-shadow-lg tracking-wide mb-10">
-                Admin Dashboard
-            </h1>
+        <div className="min-h-screen flex flex-col items-center justify-center bg-darkBlue px-4">
+            <h1 className="text-5xl font-bold text-neonBlue drop-shadow-lg tracking-wide mb-10">Admin Dashboard</h1>
         </div>
     );
 }
--- frontend/src/views/pages/EmployeeDashboard.tsx
+++ frontend/src/views/pages/EmployeeDashboard.tsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
+import {useState} from "react";
+import {Link} from "react-router-dom";
 import ApplyForLeave from "./ApplyForLeave";
 
 export default function EmployeeDashboard() {
@@ -7,23 +7,18 @@ export default function EmployeeDashboard() {
 
     return (
         <div className="min-h-screen bg-darkBlue px-4 relative">
+
             <div className="absolute right-4 top-1/2 transform -translate-y-1/2">
-                <Link
-                    to="#"
-                    onClick={(e) => {
-                        e.preventDefault();
-                        setShowPopup(true);
-                    }}
-                    className="bg-darkBlue text-neonBlue border border-neonBlue px-6 py-3 rounded-lg font-semibold shadow-lg hover:scale-105 transition duration-300"
-                >
-                    Apply For Leave
+                <Link to="#"
+                      onClick={(e) => {
+                          e.preventDefault();
+                          setShowPopup(true);
+                      }}
+                      className="bg-darkBlue text-neonBlue border border-neonBlue px-6 py-3 rounded-lg font-semibold shadow-lg hover:scale-105 transition duration-300">Apply
+                    For Leave
                 </Link>
             </div>
-
-            <div className="flex items-center justify-center min-h-screen">
-            </div>
-
-            {showPopup && <ApplyForLeave onClose={() => setShowPopup(false)} />}
+            {showPopup && <ApplyForLeave onClose={() => setShowPopup(false)}/>}
         </div>
     );
 }
